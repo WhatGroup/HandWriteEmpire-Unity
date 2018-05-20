4887df27ae18d8671ba263cba9d49c2f6c3a9bee
GreenLight <1251191746@qq.com>
2018-05-20 20:02:55 +0800

    添加过关旗子控制代码
    此版本更新内容可以直接忽略~~

---
+++ Assets/Scripts/Controller/LevelSelect/FlagStateController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlagStateController : MonoBehaviour
+{
+    public Sprite flagOne;
+    public Sprite flagTwo;
+    public Sprite flagThree;
+
+
+    private Image flagImage;
+
+    private void Start()
+    {
+        flagImage = GetComponent<Image>();
+    }
+
+    public void SetFlagIcon(int count)
+    {
+        switch (count)
+        {
+            case 1:
+                flagImage.sprite = flagOne;
+                break;
+            case 2:
+                flagImage.sprite = flagTwo;
+                break;
+            case 3:
+                flagImage.sprite = flagThree;
+                break;
+            default:
+                flagImage.sprite = flagOne;
+                break;
+        }
+    }
+}
