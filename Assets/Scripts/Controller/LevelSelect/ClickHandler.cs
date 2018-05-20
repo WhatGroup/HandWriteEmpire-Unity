ac09a29c812515e43a487e0effe81218fdf8e550
GreenLight <1251191746@qq.com>
2018-05-20 17:19:38 +0800

    添加关卡选择场景

---
+++ Assets/Scripts/Controller/LevelSelect/ClickHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickHandler : MonoBehaviour
+{
+    public void OnClickCurrenLevel()
+    {
+        BackHandler._instance.AddScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("03_Adventure");
+    }
+
+    public void OnClickOkLevel()
+    {
+        BackHandler._instance.AddScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("03_Adventure");
+    }
+
+    public void OnClickLockLevel()
+    {
+        AndroidUtil.Toast("关卡未解锁");
+    }
+}
---
+++ Assets/Scripts/Controller/LevelSelect/ItemStateController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemStateController : MonoBehaviour
+{
+    public GameObject CurrenLevel;
+    public GameObject LockLevel;
+    public GameObject OkLevel;
+
+}
