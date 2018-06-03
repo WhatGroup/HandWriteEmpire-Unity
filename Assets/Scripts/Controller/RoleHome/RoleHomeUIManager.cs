f24a1e427d5ecfac1746fd3b2a0cf746b72414ee
GreenLight <1251191746@qq.com>
2018-06-03 22:18:10 +0800

    角色之家界面搭建

---
+++ Assets/Scripts/Controller/RoleHome/RoleHomeUIManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoleHomeUIManager : MonoBehaviour
+{
+    public Text attackValue;
+
+    public Text defenseValue;
+
+    public Text cureValue;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (UserInfoManager._instance.GetUserInfo() != null)
+        {
+            UpdateUserInfo();
+        }
+    }
+
+    private void UpdateUserInfo()
+    {
+        var userInfo = UserInfoManager._instance.GetUserInfo();
+        attackValue.text = userInfo.attackValue + "";
+        defenseValue.text = userInfo.defenseValue + "";
+        cureValue.text = userInfo.cureValue + "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
