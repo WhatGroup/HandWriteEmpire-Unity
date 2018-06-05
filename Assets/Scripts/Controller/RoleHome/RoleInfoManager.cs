d8adad0708249a6088664dd1c94af6ff4ba5f354
GreenLight <1251191746@qq.com>
2018-06-05 09:16:25 +0800

    添加角色之家的UI

---
+++ Assets/Scripts/Controller/RoleHome/RoleInfoManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoleInfoManager : MonoBehaviour
+{
+    public Image roleLiHui;
+
+    public Text hpValue;
+
+    public Image roleTypeSeal;
+
+    public Text roleIntro;
+
+    public Text roleSkillDesc;
+
+    public Text roleName;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
