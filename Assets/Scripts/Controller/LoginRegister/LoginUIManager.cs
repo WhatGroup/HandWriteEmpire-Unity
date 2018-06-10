eb8328e153704d9edf04d74a2b77d91718d0b73e
GreenLight <1251191746@qq.com>
2018-06-10 22:20:40 +0800

    修改登录注册界面
    登录注册合并为一个界面

---
+++ Assets/Scripts/Controller/LoginRegister/LoginUIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginUIManager : MonoBehaviour
+{
+    public GameObject registerPanelGo;
+
+    public void OnClickShowRegisterPanleBtn()
+    {
+        registerPanelGo.SetActive(true);
+    }
+
+    public void OnClickCloseRegisterPanelBtn()
+    {
+        registerPanelGo.SetActive(false);
+    }
+}
