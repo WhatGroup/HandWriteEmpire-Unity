3790987e4f17ccf84785fb9a860c92450efad915
GreenLight <1251191746@qq.com>
2018-06-12 19:13:50 +0800

    角色选择场景添加拖动效果
    注:也就只有拖动的效果

---
+++ Assets/Scripts/Controller/RoleHome/RoleDrag.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RoleDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public RectTransform canvas;
+
+    public Image portrait;
+
+    private Image liHui;
+
+    public Sprite sprite;
+    
+
+    private Vector2 offset = new Vector3();
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Vector2 mouseDown = eventData.position;
+        Vector2 mouseUGUIPos = new Vector2();
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas, mouseDown, eventData.enterEventCamera,
+                out mouseUGUIPos);
+        liHui = Instantiate(portrait);
+        liHui.sprite = sprite;
+        liHui.SetNativeSize();
+        liHui.rectTransform.SetParent(canvas);
+        liHui.rectTransform.anchoredPosition = mouseUGUIPos;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 mouseDrag = eventData.position;
+        Vector2 uguiPos = new Vector2();
+        bool isRect =
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas, mouseDrag, eventData.enterEventCamera,
+                out uguiPos);
+        if (isRect)
+        {
+            liHui.rectTransform.anchoredPosition = offset + uguiPos;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Destroy(liHui.gameObject);
+    }
+}
